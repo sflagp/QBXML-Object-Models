f205ac42d29178ce80122c8b343dffaa734bb6f8
Greg <56395390+sflagp@users.noreply.github.com>
2022-03-24 17:58:01 -0400

    Create SampleInvoicesUnitTest.cs
    This is one of my Unit Tests that I created to test and find errors in the QBXML.  The QbProcessor is my own personal Quickbooks requests processor which is sending the xml.  You will need to use your own requests processor for that.  However; the resulting string is then processed through the included ToView method to copy the xml response into a complete C# object.  You can then access the QBXML response as you would any other C# object.

---
+++ SampleInvoicesUnitTest.cs
@@ -0,0 +1,120 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QbModels;
+using System;
+using System.Threading;
+
+namespace QbProcessor.TEST
+{
+    [TestClass]
+    public class InvoiceTests
+    {
+        [TestMethod]
+        public void TestInvoiceModels()
+        {
+            using (QBProcessor.QbProcessor QB = new())
+            {
+                #region Properties
+                if (QB == null)
+                {
+                    throw new Exception("Quickbooks not loaded or error connecting to Quickbooks.");
+                }
+
+                QbInvoicesView qryRs, addRs = new(), modRs;
+                InvoiceAddRq addRq = new();
+                InvoiceModRq modRq = new();
+                string addRqName = $"QbProcessor";
+                string result;
+                #endregion
+
+                #region Query Test
+                InvoiceQueryRq qryRq = new();
+                Assert.IsTrue(qryRq.IsEntityValid());
+
+                qryRq.RefNumberFilter = new() { RefNumber = addRqName, MatchCriterion = "StartsWith" };
+                Assert.IsTrue(qryRq.IsEntityValid());
+
+                result = QB.ExecuteQbRequest(qryRq);
+                qryRs = QB.ToView<QbInvoicesView>(result);
+                Assert.IsTrue(qryRs.StatusSeverity == "Info");
+                #endregion
+
+                #region Add Test
+                if (qryRs.TotalInvoices == 0)
+                {
+                    Random rdm = new();
+
+                    AccountQueryRq accountsRq = new();
+                    accountsRq.AccountType = "AccountsReceivable";
+                    QbAccountsView accounts = QB.ToView<QbAccountsView>(QB.ExecuteQbRequest(accountsRq));
+                    AccountRetDto account = accounts.Accounts[rdm.Next(0, accounts.Accounts.Count)];
+
+                    CustomerQueryRq customerRq = new();
+                    QbCustomersView customers = QB.ToView<QbCustomersView>(QB.ExecuteQbRequest(customerRq));
+                    CustomerRetDto customer = customers.Customers[rdm.Next(0, customers.Customers.Count)];
+
+                    ItemNonInventoryQueryRq itemsRq = new();
+                    QbItemNonInventoryView items = QB.ToView<QbItemNonInventoryView>(QB.ExecuteQbRequest(itemsRq));
+
+                    ItemOtherChargeQueryRq chargeRq = new();
+                    QbItemOtherChargesView charges = QB.ToView<QbItemOtherChargesView>(QB.ExecuteQbRequest(chargeRq));
+
+                    addRq.Customer = new() { ListID = customer.ListID };
+                    addRq.ARAccount = new() { ListID = account.ListID };
+                    addRq.TxnDate = DateTime.Now;
+                    addRq.RefNumber = addRqName;
+                    addRq.InvoiceLine = new();
+                    addRq.InvoiceLine.Add(new()
+                    {
+                        Item = new() { ListID = items.ItemsNonInventory[rdm.Next(0, items.ItemsNonInventory.Count)].ListID },
+                        Rate = 12.34M,
+                        Quantity = 5,
+                        Desc = $"#1 QbProcessor.{addRq.GetType().Name} on {DateTime.Now}"
+                    });
+                    addRq.InvoiceLine.Add(new()
+                    {
+                        Item = new() { ListID = items.ItemsNonInventory[rdm.Next(0, items.ItemsNonInventory.Count)].ListID },
+                        Rate = 20M,
+                        Quantity = 1,
+                        Desc = $"#2 QbProcessor.{addRq.GetType().Name} on {DateTime.Now}"
+                    });
+                    addRq.InvoiceLine.Add(new()
+                    {
+                        Item = new() { ListID = charges.ItemOtherCharges[rdm.Next(0, charges.ItemOtherCharges.Count)].ListID },
+                        Rate = 250,
+                        Amount = 123.45M,
+                        Desc = $"#4 QbProcessor.{addRq.GetType().Name} on {DateTime.Now}"
+                    });
+                    addRq.InvoiceLine.Add(new()
+                    {
+                        Item = new() { ListID = items.ItemsNonInventory[rdm.Next(0, items.ItemsNonInventory.Count)].ListID },
+                        Rate = 11M,
+                        Quantity = 30,
+                        Desc = $"#3 QbProcessor.{addRq.GetType().Name} on {DateTime.Now}"
+                    });
+                    Assert.IsTrue(addRq.IsEntityValid());
+
+                    result = QB.ExecuteQbRequest(addRq);
+                    addRs = QB.ToView<QbInvoicesView>(result);
+                    Assert.IsTrue(addRs.StatusCode == "0");
+                }
+                #endregion
+
+                #region Mod Test
+                InvoiceRetDto Invoice = qryRs.TotalInvoices == 0 ? addRs.Invoices[0] : qryRs.Invoices[0];
+                modRq.TxnID = Invoice.TxnID;
+                modRq.EditSequence = Invoice.EditSequence;
+                modRq.TxnDate = Invoice.TxnDate;
+                modRq.Customer = Invoice.Customer;
+                modRq.Memo = $"QbProcessor.{modRq.GetType().Name} on {DateTime.Now}";
+                Assert.IsTrue(modRq.IsEntityValid());
+
+                modRq.TxnDate = default;
+                result = QB.ExecuteQbRequest(modRq);
+                modRs = QB.ToView<QbInvoicesView>(result);
+                Assert.IsTrue(modRs.StatusCode == "0");
+                #endregion
+            }
+            Thread.Sleep(2000);
+        }
+    }
+}
